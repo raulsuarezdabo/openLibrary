d0d91e89e864132932db27fbfb3f56ac88817bc7
Raul Suarez Dabo <raul@suarez.com.es>
2016-02-05 17:24:49 +0100

    Basic structure

--- openLibrary/ViewController.swift
+++ openLibrary/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
 
+    @IBOutlet weak var textView: UITextView!
+    // Search argument
+    @IBOutlet weak var search: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        search.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        let url = NSURL(string: self.urls + searchBar.text!)
+        let datos: NSData? = NSData(contentsOfURL: url!)
+        self.textView.text = String(NSString(data: datos!, encoding:NSUTF8StringEncoding))
+    }
 
+    @IBAction func clearSearch() {
+        self.search.text = ""
+    }
 
+    
 }
 
